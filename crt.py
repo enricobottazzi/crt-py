4cfa58960229739db132f43c69a64258da2dec32
enrico.eth <enricobottazzi@icloud.com>
2023-12-29 16:45:07 +0100

    feat: add `CRTInteger` and `CRTPolynomial`

--- crt.py
+++ crt.py
@@ -1,16 +1,131 @@
 import math
+from typing import List
 
 
 class Q:
-    def __init__(self, qis: list):
+    def __init__(self, qis: List[int]):
+        q = 1
         # qis should be same size single precision integers of 60 bits (or less)
         for qi in qis:
             assert isinstance(qi, int)
             assert qi.bit_length() <= 60, "qi is too large"
+            q *= qi
 
-        # assert that qis are pairwise coprime
+        # qis should be coprime
         for i in range(len(qis)):
             for j in range(i + 1, len(qis)):
                 assert math.gcd(qis[i], qis[j]) == 1, "qis are not pairwise coprime"
 
         self.qis = qis
+        self.q = q
+
+
+class CRTInteger:
+    def __init__(self, q: Q, xis: List[int]):
+        self.q = q
+        self.xis = xis
+
+    def from_crt_components(q: Q, xis: List[int]):
+        return CRTInteger(q, xis)
+
+    def from_integer(q: Q, x: int):
+        xis = []
+        for qi in q.qis:
+            xis.append(x % qi)
+
+        return CRTInteger(q, xis)
+
+    def recover(self):
+        x = 0
+        for i in range(len(self.q.qis)):
+            xi = self.xis[i]
+            qi_star = self.q.q // self.q.qis[i]
+            qi_tilde = pow(
+                qi_star, -1, self.q.qis[i]
+            )  # inverse of qi_star mod self.q.qis[i]
+            x += xi * qi_star * qi_tilde
+
+        return x % self.q.q
+
+    def __add__(self, other):
+        assert isinstance(other, CRTInteger)
+        assert self.q == other.q, "q is not the same"
+        assert len(self.xis) == len(other.xis), "xis are not the same size"
+
+        xis = []
+        for i in range(len(self.xis)):
+            xis.append((self.xis[i] + other.xis[i]) % self.q.qis[i])
+
+        return CRTInteger(self.q, xis)
+
+    def __mul__(self, other):
+        assert isinstance(other, CRTInteger)
+        assert self.q == other.q, "q is not the same"
+        assert len(self.xis) == len(other.xis), "xis are not the same size"
+
+        xis = []
+        for i in range(len(self.xis)):
+            xis.append((self.xis[i] * other.xis[i]) % self.q.qis[i])
+
+        return CRTInteger(self.q, xis)
+
+
+class CRTPolynomial:
+    def __init__(self, q: Q, coefficients: List[CRTInteger]):
+        self.q = q
+        self.coefficients = coefficients
+
+    def from_crt_components_coefficients(
+        q: Q, coefficients_crt_componenents: List[List[int]]
+    ):
+        coefficients = []
+        for coefficient_crt_components in coefficients_crt_componenents:
+            coefficients.append(
+                CRTInteger.from_crt_components(q, coefficient_crt_components)
+            )
+
+        return CRTPolynomial(q, coefficients)
+
+    def from_int_coefficients(q: Q, coefficients_int: List[int]):
+        coefficients = []
+        for coefficient_int in coefficients_int:
+            coefficients.append(CRTInteger.from_integer(q, coefficient_int))
+
+        return CRTPolynomial(q, coefficients)
+
+    def recover(self):
+        coefficients = []
+        for i in range(len(self.coefficients)):
+            coefficients.append(self.coefficients[i].recover())
+
+        return coefficients
+
+    def __add__(self, other):
+        assert isinstance(other, CRTPolynomial)
+        assert self.q == other.q, "q is not the same"
+        assert len(self.coefficients) == len(
+            other.coefficients
+        ), "coefficients are not the same size"
+
+        coefficients = []
+        for i in range(len(self.coefficients)):
+            coefficients.append(self.coefficients[i] + other.coefficients[i])
+
+        return CRTPolynomial(self.q, coefficients)
+
+    def __mul__(self, other):
+        assert isinstance(other, CRTPolynomial)
+        assert self.q == other.q, "q is not the same"
+        assert len(self.coefficients) == len(
+            other.coefficients
+        ), "coefficients are not the same size"
+
+        coefficients = []
+        for i in range(len(self.coefficients)):
+            for j in range(len(self.coefficients)):
+                if i + j < len(coefficients):
+                    coefficients[i + j] += self.coefficients[i] * other.coefficients[j]
+                else:
+                    coefficients.append(self.coefficients[i] * other.coefficients[j])
+
+        return CRTPolynomial(self.q, coefficients)
--- crt_test.py
+++ crt_test.py
@@ -1,5 +1,6 @@
 import unittest
-from crt import Q
+from crt import Q, CRTInteger, CRTPolynomial
+import numpy as np
 
 
 class TestQ(unittest.TestCase):
@@ -17,6 +18,113 @@ class TestQ(unittest.TestCase):
         qis = [2, 3, 2**61]
         with self.assertRaisesRegex(AssertionError, "qi is too large"):
             Q(qis)
+
+
+class TestCRTInteger(unittest.TestCase):
+    def test_from_crt_components_valid(
+        self,
+    ):  # from tutorial https://www.youtube.com/watch?v=zIFehsBHB8o
+        qis = [5, 7, 8]
+        q = Q(qis)
+        xis = [3, 1, 6]
+        crt_integer = CRTInteger.from_crt_components(q, xis)
+        self.assertEqual(crt_integer.recover(), 78)
+
+    def test_from_integer_valid(self):
+        qis = [5, 7, 8]
+        q = Q(qis)
+        x = 78
+        crt_integer = CRTInteger.from_integer(q, x)
+        self.assertEqual(crt_integer.recover(), 78)
+
+    def test_add_valid(self):
+        qis = [5, 7, 8]
+        q = Q(qis)
+        xis1 = [3, 1, 6]
+        xis2 = [4, 2, 2]
+        crt_integer_1 = CRTInteger.from_crt_components(q, xis1)
+        crt_integer_2 = CRTInteger.from_crt_components(q, xis2)
+        crt_integer_3 = crt_integer_1 + crt_integer_2
+        recovered_1 = crt_integer_1.recover()
+        recovered_2 = crt_integer_2.recover()
+        recovered_3 = crt_integer_3.recover()
+        self.assertEqual(recovered_1 + recovered_2 % q.q, recovered_3)
+
+    def test_mul_valid(self):
+        qis = [5, 7, 8]
+        q = Q(qis)
+        xis1 = [3, 1, 6]
+        xis2 = [4, 2, 2]
+        crt_integer_1 = CRTInteger.from_crt_components(q, xis1)
+        crt_integer_2 = CRTInteger.from_crt_components(q, xis2)
+        crt_integer_3 = crt_integer_1 * crt_integer_2
+        recovered_1 = crt_integer_1.recover()
+        recovered_2 = crt_integer_2.recover()
+        recovered_3 = crt_integer_3.recover()
+        self.assertEqual(recovered_1 * recovered_2 % q.q, recovered_3)
+
+
+class TestCRTPolynomial(unittest.TestCase):
+    def test_from_crt_components_coefficients_valid(
+        self,
+    ):
+        qis = [5, 7, 8]
+        q = Q(qis)
+        coeff_1 = [3, 1, 6]
+        coeff_2 = [4, 2, 2]
+        coeffs = [coeff_1, coeff_2]
+        crt_poly = CRTPolynomial.from_crt_components_coefficients(q, coeffs)
+        self.assertEqual(crt_poly.coefficients[0].recover(), 78)
+        self.assertEqual(crt_poly.coefficients[1].recover(), 114)
+
+    def test_from_crt_components_polynomial_valid(
+        self,
+    ):
+        qis = [5, 7, 8]
+        q = Q(qis)
+        coeff_1 = 78
+        coeff_2 = 114
+        coeffs = [coeff_1, coeff_2]
+        crt_poly = CRTPolynomial.from_int_coefficients(q, coeffs)
+        self.assertEqual(crt_poly.coefficients[0].recover(), 78)
+        self.assertEqual(crt_poly.coefficients[1].recover(), 114)
+
+    def test_add_valid(self):
+        qis = [5, 7, 8]
+        q = Q(qis)
+        coeff_1 = [3, 1, 6]
+        coeff_2 = [4, 2, 2]
+        coeffs = [coeff_1, coeff_2]
+        crt_poly_1 = CRTPolynomial.from_crt_components_coefficients(q, coeffs)
+        crt_poly_2 = CRTPolynomial.from_crt_components_coefficients(q, coeffs)
+        poly_1_recovered_coeffs = crt_poly_1.recover()
+        poly_2_recovered_coeffs = crt_poly_2.recover()
+        poly_1 = np.poly1d(poly_1_recovered_coeffs)
+        poly_2 = np.poly1d(poly_2_recovered_coeffs)
+        poly_3 = poly_1 + poly_2
+        poly_3_coeffs = poly_3.coefficients.tolist()
+        poly_3_coeffs = [coeff % q.q for coeff in poly_3_coeffs]
+        crt_poly_3 = crt_poly_1 + crt_poly_2
+        poly_3_recovered_coeffs = crt_poly_3.recover()
+        self.assertEqual(poly_3_recovered_coeffs, poly_3_coeffs)
+
+    def test_mul_valid(self):
+        qis = [5, 7, 8]
+        q = Q(qis)
+        coeff_1 = [3, 1, 6]
+        coeff_2 = [4, 2, 2]
+        coeffs = [coeff_1, coeff_2]
+        crt_poly_1 = CRTPolynomial.from_crt_components_coefficients(q, coeffs)
+        crt_poly_2 = CRTPolynomial.from_crt_components_coefficients(q, coeffs)
+        poly_1_recovered_coeffs = crt_poly_1.recover()
+        poly_2_recovered_coeffs = crt_poly_2.recover()
+        poly_1 = np.poly1d(poly_1_recovered_coeffs)
+        poly_2 = np.poly1d(poly_2_recovered_coeffs)
+        poly_3 = poly_1 * poly_2
+        poly_3_coeffs = [coeff % q.q for coeff in poly_3]
+        crt_poly_3 = crt_poly_1 * crt_poly_2
+        poly_3_recovered_coeffs = crt_poly_3.recover()
+        self.assertEqual(poly_3_recovered_coeffs, poly_3_coeffs)
 
 
 if __name__ == "__main__":
